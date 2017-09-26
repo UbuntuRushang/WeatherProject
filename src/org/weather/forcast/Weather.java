8e7f05887896e18d3372ee18cc06a8650e6c19b1
UbuntuRushang <ubuntu.rushang@gmail.com>
2017-09-26 00:41:16 -0400

    Weather Project

---
+++ src/org/weather/forcast/Weather.java
@@ -0,0 +1,13 @@
+package org.weather.forcast;
+
+public class Weather{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Today's weather forecast is Sunny..");
+
+	}
+
+}
