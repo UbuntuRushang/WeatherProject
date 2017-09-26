21c1a87927e36cce9e79632a040bb3a96a8cd024
UbuntuRushang <ubuntu.rushang@gmail.com>
2017-09-26 00:54:40 -0400

    Its Hot added

--- src/org/weather/forcast/Weather.java
+++ src/org/weather/forcast/Weather.java
@@ -6,7 +6,7 @@ public class Weather{
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Today's weather forecast is Sunny..");
+		System.out.println("Today's weather forecast is Hot..");
 
 	}
 
